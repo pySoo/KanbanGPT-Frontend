2c70af41ca91d7bf734172609aeca1920fda7d59
pySoo <soopy368@gmail.com>
2023-07-20 01:44:09 +0900

    [feat] : 프로젝트 초기 세팅

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [react()],
+});
