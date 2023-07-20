762aaf2271958cd34cce5a56b431cbc2d006a9e7
pySoo <soopy368@gmail.com>
2023-07-20 15:34:26 +0900

    [feat] : theme colors 상수 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+const palette = {
+  primary: '#064061',
+  secondary: '#4EB7F2',
+  neutral: {
+    100: '#f5f5f5',
+    200: '#e5e5e5',
+    300: '#d4d4d4',
+    400: '#a3a3a3',
+    500: '#737373',
+    600: '#525252',
+    700: '#404040',
+    800: '#262626',
+    900: '#171717',
+  },
+};
+
+export const theme = {
+  colors: palette,
+};
