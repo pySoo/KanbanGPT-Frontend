238cb0a77cf2930dfc185c3e3232a225caaa7207
pySoo <soopy368@gmail.com>
2023-07-20 15:35:14 +0900

    [feat] : Global Style과 Theme 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Global, ThemeProvider } from '@emotion/react';
+
+import { reset } from './styles/reset';
+import { theme } from './styles/theme';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={reset} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
