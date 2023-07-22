11118811080f3f7f0156a6f4f7254b04dc6cfeaf
pySoo <soopy368@gmail.com>
2023-07-22 19:00:51 +0900

    [feat] : modal type 선언

---
+++ src/types/modal.ts
@@ -0,0 +1,13 @@
+export type ModalStateType = {
+  type: ModalType;
+  props?: React.ComponentProps<'div'>;
+};
+
+export type ModalDispatchType = {
+  openModal: ({ type, props }: ModalStateType) => void;
+  closeModal: ({ type }: ModalStateType) => void;
+};
+
+export enum ModalType {
+  ISSUE = 'ISSUE',
+}
