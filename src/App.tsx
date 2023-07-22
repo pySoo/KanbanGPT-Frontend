5a59dea12243516bc34e8772e8884d9eb87d6fa4
pySoo <soopy368@gmail.com>
2023-07-22 19:05:16 +0900

    [refactor] : Provider 코드 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Global, ThemeProvider } from '@emotion/react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { path } from './constants/path';
 import Layout from './pages/_layout';
 import Main from './pages/Main';
-import { reset } from './styles/reset';
-import { theme } from './styles/theme';
 
 function App() {
   const router = createBrowserRouter([
@@ -16,12 +13,7 @@ function App() {
     },
   ]);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Global styles={reset} />
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
