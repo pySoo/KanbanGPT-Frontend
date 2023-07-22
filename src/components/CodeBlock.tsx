8ad488f44a24b1a1d49fe0913115732bc92e8627
pySoo <soopy368@gmail.com>
2023-07-22 16:33:17 +0900

    [feat] : 코드 복사 기능 추가

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,20 +1,33 @@
 import { css } from '@emotion/react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import { gptCode } from '@/data/gpt';
 
+import CheckIcon from './icons/CheckIcon';
 import CopyIcon from './icons/CopyIcon';
 
 export default function CodeBlock() {
+  const [isCopied, setIsCopied] = useState(false);
   const codeBlockRef = useRef<HTMLDivElement | null>(null);
   const language = 'typescript';
 
+  const handleCodeCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(gptCode);
+      setIsCopied(true);
+      alert('링크를 복사했습니다.');
+    } catch (e) {
+      console.error(e);
+      alert('링크 복사에 실패했습니다.');
+    }
+  };
+
   return (
     <div ref={codeBlockRef} css={codeBlockStyle}>
-      <button css={copyButtonStyle}>
-        <CopyIcon />
+      <button css={copyButtonStyle(isCopied)} onClick={handleCodeCopy}>
+        {isCopied ? <CheckIcon /> : <CopyIcon />}
       </button>
       <SyntaxHighlighter customStyle={customStyle} language={language} style={darcula}>
         {gptCode}
@@ -35,10 +48,14 @@ const customStyle = {
   margin: 0,
 };
 
-const copyButtonStyle = css`
+const copyButtonStyle = (isCopied: boolean) => css`
   position: absolute;
   right: 0;
   color: white;
   padding: 20px;
   transition: all 0.2s ease-in-out;
+  scale: ${isCopied ? '1.6' : '1'};
+  :hover {
+    transform: ${isCopied ? 'scale(1)' : 'scale(1.2)'};
+  }
 `;
