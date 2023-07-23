9bd13f581939521f95ad57c6f2693ce1d8f0c338
pySoo <soopy368@gmail.com>
2023-07-23 16:31:27 +0900

    [feat] : Issue Provider 적용

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from '@emotion/react';
 import { ReactNode } from 'react';
 
+import IssueProvider from './contexts/Issue/IssueProvider';
 import ModalProvider from './contexts/Modal/ModalProvider';
 import { theme } from './styles/theme';
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider theme={theme}>
-      <ModalProvider>{children}</ModalProvider>
+      <IssueProvider>
+        <ModalProvider>{children}</ModalProvider>
+      </IssueProvider>
     </ThemeProvider>
   );
 }
