260542d6bf2a53f79079ce37b157d7abb39ad62f
pySoo <soopy368@gmail.com>
2023-07-23 21:00:35 +0900

    [feat] : 이슈 create, blur, submit 동작 추가

--- src/components/KanbanBoard/KanbanCard.tsx
+++ src/components/KanbanBoard/KanbanCard.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import { useState } from 'react';
 
 import { IssueStateType } from '@/types/issue';
 
@@ -13,12 +14,27 @@ type KanbanCardProps = {
 };
 
 export default function KanbanCard({ title, labelColor, issueList }: KanbanCardProps) {
+  const [isCreateIssue, setIsCreateIssue] = useState(false);
+
+  const handleCreateBtnClick = () => {
+    setIsCreateIssue(true);
+  };
+
+  const handleIssueBlur = () => {
+    setIsCreateIssue(false);
+  };
+
+  const handleIssueSubmit = () => {
+    setIsCreateIssue(false);
+  };
+
   return (
     <div css={kanbanLayoutStyle}>
       <div css={kanbanCardStyle}>
         <Label bgColor={labelColor}>{title}</Label>
-        <CreateIssueBtn />
         {issueList?.map((issue) => <IssueMemo key={issue.id} issue={issue} />)}
+        {isCreateIssue && <IssueMemo onBlur={handleIssueBlur} onSubmit={handleIssueSubmit} />}
+        <CreateIssueBtn onClick={handleCreateBtnClick} />
       </div>
     </div>
   );
@@ -37,7 +53,7 @@ const kanbanCardStyle = css`
   align-items: start;
   gap: 10px;
   padding: 10px;
-  padding-bottom: 20px;
+  padding-bottom: 15px;
   border-radius: 8px;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   margin-top: 15px;
