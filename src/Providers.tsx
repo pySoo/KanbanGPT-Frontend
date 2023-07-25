c16471217f2e086cc3f7752539d7eb2080b4a435
pySoo <soopy368@gmail.com>
2023-07-25 14:56:44 +0900

    [feat] : context provider 제거, recoil root 추가

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,16 +1,13 @@
 import { ThemeProvider } from '@emotion/react';
 import { ReactNode } from 'react';
+import { RecoilRoot } from 'recoil';
 
-import IssueProvider from './contexts/Issue/IssueProvider';
-import ModalProvider from './contexts/Modal/ModalProvider';
 import { theme } from './styles/theme';
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider theme={theme}>
-      <IssueProvider>
-        <ModalProvider>{children}</ModalProvider>
-      </IssueProvider>
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </RecoilRoot>
   );
 }
