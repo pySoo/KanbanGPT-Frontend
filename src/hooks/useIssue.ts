41dbd147ccc582f8f33729e40588f5d8fe912ad7
pySoo <soopy368@gmail.com>
2023-07-25 16:45:12 +0900

    [feat] : issue, requirement의 crud를 담당하는 hook 추가

---
+++ src/hooks/useIssue.ts
@@ -0,0 +1,40 @@
+import { useRecoilState } from 'recoil';
+
+import { issueAtom } from '@/atoms/issueAtom';
+import { createIssueProps, IssueStateType, updateIssueProps } from '@/types/issue';
+import { createUniqueId } from '@/utils/uniqueId';
+
+export function useIssue() {
+  const [issueList, setIssueList] = useRecoilState<IssueStateType[]>(issueAtom);
+
+  const createIssue = ({ status, title }: createIssueProps) => {
+    const id = createUniqueId();
+    setIssueList((prevIssue) => [
+      ...prevIssue,
+      {
+        status,
+        id,
+        title,
+      },
+    ]);
+  };
+
+  const updateIssue = ({ id, status, title }: updateIssueProps) => {
+    const updatedIssue = issueList.map((issue) =>
+      issue.id === id
+        ? {
+            ...issue,
+            status: status ?? issue.status,
+            title: title ?? issue.title,
+          }
+        : issue,
+    );
+    setIssueList(updatedIssue);
+  };
+
+  const deleteIssue = ({ id }: { id: string }) => {
+    setIssueList(issueList.filter((issue) => issue.id !== id));
+  };
+
+  return { issueList, createIssue, updateIssue, deleteIssue };
+}
