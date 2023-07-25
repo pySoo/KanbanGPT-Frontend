7c9078bab3a8a7e140f246b1562cf66bbab107f9
pySoo <soopy368@gmail.com>
2023-07-25 21:11:12 +0900

    [feat] : modal atom에도 localStorage 연동

--- src/atoms/modalAtom.ts
+++ src/atoms/modalAtom.ts
@@ -1,8 +1,11 @@
 import { atom } from 'recoil';
 
+import { localStorageKey } from '@/constants/localStorage';
 import { ModalStateType } from '@/types/modal';
+import { localStorageEffect } from '@/utils/localStorage';
 
 export const modalAtom = atom<ModalStateType[]>({
   key: 'modalState',
   default: [],
+  effects: [localStorageEffect<ModalStateType[]>(localStorageKey.modal)],
 });
