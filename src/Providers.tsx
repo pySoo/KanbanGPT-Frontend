85a1cec7d48a0657196bb1152f3353843dab6c1b
pySoo <soopy368@gmail.com>
2023-07-29 01:28:59 +0900

    [feat] : react-query provider 설정

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from '@emotion/react';
 import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
 import { theme } from './styles/theme';
 
+const queryClient = new QueryClient();
+
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
