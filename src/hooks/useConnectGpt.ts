6a65242ec42f476d16c761b1b9ae0b0243f28e6c
pySoo <soopy368@gmail.com>
2023-07-29 01:31:39 +0900

    [feat] : apiKey 반환 코드 추가

--- src/hooks/useConnectGpt.ts
+++ src/hooks/useConnectGpt.ts
@@ -6,14 +6,15 @@ import { gptAtom } from '@/atoms/gptAtom';
 export default function useConnectGpt() {
   const [isConnected, setIsConnected] = useState(false);
   const gptState = useRecoilValue(gptAtom);
+  const apiKey = gptState.key;
 
   useEffect(() => {
-    if (gptState.key) {
+    if (apiKey) {
       setIsConnected(true);
     } else {
       setIsConnected(false);
     }
   }, [gptState]);
 
-  return { isConnected };
+  return { isConnected, apiKey };
 }
