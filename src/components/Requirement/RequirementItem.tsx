08a37bd86641bbfafb6e130e539abd22aa7c8db0
pySoo <soopy368@gmail.com>
2023-07-30 02:57:05 +0900

    [refactor] : 토글 체크 버튼에서 쓰였던 로직, 상태 제거

--- src/components/Requirement/RequirementItem.tsx
+++ src/components/Requirement/RequirementItem.tsx
@@ -1,13 +1,10 @@
 import { css } from '@emotion/react';
-import { useState } from 'react';
 
 import { useRequirement } from '@/hooks/useRequirement';
 import { RequirementStateType } from '@/types/requirement';
 
-import CheckLottie from '../common/CheckLottie';
-import CheckCircleIcon from '../icons/CheckCircleIcon';
 import DeleteIcon from '../icons/DeleteIcon';
-import EmptyCircleIcon from '../icons/EmptyCircleIcon';
+import RequirementCheckToggle from './RequirementCheckToggle';
 import RequirementInput from './RequirementInput';
 
 type RequirementItemProps = {
@@ -16,17 +13,7 @@ type RequirementItemProps = {
 };
 
 export default function RequirementItem({ requirement, onSelectId }: RequirementItemProps) {
-  const { isCompleted } = requirement;
-  const { updateRequire, deleteRequire } = useRequirement();
-
-  const [isClicked, setIsClicked] = useState(false);
-  const [isChecked, setIsChecked] = useState(isCompleted);
-
-  const handleToggleChecked = () => {
-    !isClicked && setIsClicked(true);
-    setIsChecked(!isChecked);
-    updateRequire({ ...requirement, isCompleted: !isChecked });
-  };
+  const { deleteRequire } = useRequirement();
 
   const handleRequireDelete = () => {
     deleteRequire({ id: requirement.id });
@@ -35,10 +22,7 @@ export default function RequirementItem({ requirement, onSelectId }: Requirement
   return (
     <li css={requirementItemStyle}>
       <div css={titleSectionStyle}>
-        <div css={checkboxStyle} onClick={handleToggleChecked}>
-          {!isClicked && (isChecked ? <CheckCircleIcon /> : <EmptyCircleIcon />)}
-          {isClicked && (isChecked ? <CheckLottie /> : <EmptyCircleIcon />)}
-        </div>
+        <RequirementCheckToggle requirement={requirement} />
         <RequirementInput
           issueId={requirement.issueId}
           requirement={requirement}
@@ -62,12 +46,6 @@ const requirementItemStyle = css`
 const titleSectionStyle = css`
   display: flex;
   gap: 10px;
-`;
-
-const checkboxStyle = css`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
 `;
 
 const deleteBtnStyle = css`
