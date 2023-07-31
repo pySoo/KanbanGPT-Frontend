406a66e814c85e06beec8ace2c8440b62ece75a8
pySoo <soopy368@gmail.com>
2023-07-31 14:41:04 +0900

    [style] : GPT Info 모달 컴포넌트 스타일링

--- src/components/gpt/info/APISetupSection.tsx
+++ src/components/gpt/info/APISetupSection.tsx
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
 
+import RoundedBtn from '@/components/common/RoundedBtn';
+
 import APIKeyForm from './APIKeyForm';
 
 export default function APISetupSection() {
@@ -10,12 +12,10 @@ export default function APISetupSection() {
   return (
     <div css={apiSetupSectionStyle}>
       <div className="setup-section">
-        <h2>GPT API 연동하기</h2>
-        <button type="button" onClick={handleOpenApiPage}>
-          API Key 발급받기
-        </button>
+        <h2>GPT API 연동</h2>
+        <RoundedBtn onClick={handleOpenApiPage}>API Key 발급 받기</RoundedBtn>
       </div>
-      <span>API Key를 등록하면 요구사항을 구현한 코드를 바로 받아볼 수 있어요.</span>
+      <span>• API Key를 등록하면 요구사항을 구현한 코드를 바로 받아볼 수 있어요.</span>
       <APIKeyForm />
     </div>
   );
