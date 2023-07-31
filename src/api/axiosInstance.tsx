4493ccb2da2db68297b169189950c81f8d576ed9
pySoo <soopy368@gmail.com>
2023-08-01 01:28:50 +0900

    [fix] : undefined 에러 대응

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -14,13 +14,14 @@ const onResponse = (response: AxiosResponse): AxiosResponse => {
 
 const onError = (error: AxiosError) => {
   const response = error.response as AxiosResponse;
-  const { data } = response;
   switch (response.status) {
     case 401:
       toast.error(<ConnectToGptModal />, { autoClose: false });
       break;
     default:
-      toast.error(data.error);
+      if (response.data && response.data.error) {
+        toast.error(response.data.error);
+      }
       break;
   }
   return Promise.reject(error);
