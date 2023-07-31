c06da2f07fed502b563458aa9862c2e6dae72b81
pySoo <soopy368@gmail.com>
2023-07-31 14:25:48 +0900

    [feat] : CodeArchive 페이지 컴포넌트 추가

---
+++ src/pages/CodeArchive.tsx
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+
+import CodeArchiveList from '@/components/CodeArchive/CodeArchiveList';
+import { theme, ThemeType } from '@/styles/theme';
+
+export default function CodeArchive() {
+  return (
+    <div css={codeArchiveStyle(theme)}>
+      <h1 className="code-archive-title">CODE ARCHIVE</h1>
+      <CodeArchiveList />
+    </div>
+  );
+}
+
+const codeArchiveStyle = (theme: ThemeType) => css`
+  width: 100vw;
+  height: calc(100vh - 56px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  color: ${theme.colors.text};
+
+  .code-archive-title {
+    font-size: 1.6rem;
+    margin-bottom: 10px;
+    font-weight: 700;
+    color: ${theme.colors.done};
+    :hover {
+      color: ${theme.colors.primary};
+    }
+  }
+`;
