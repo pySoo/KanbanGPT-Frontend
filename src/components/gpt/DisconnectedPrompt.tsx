4f5cd2deb963bdc7d75a31c1ad53080b6df04a84
pySoo <soopy368@gmail.com>
2023-07-31 14:20:41 +0900

    [feat] : api key 등록 상태에 따라 gpt 안내 화면 보여주기

--- src/components/gpt/DisconnectedPrompt.tsx
+++ src/components/gpt/DisconnectedPrompt.tsx
@@ -1,62 +1,50 @@
 import { css } from '@emotion/react';
-import { toast } from 'react-toastify';
 
-import { useModal } from '@/hooks/useModal';
-import { ModalType } from '@/types/modal';
+import { theme, ThemeType } from '@/styles/theme';
 import { RequirementStateType } from '@/types/requirement';
+
+import CodeBlock from './CodeBlock';
+import GPTAPIKeyInfo from './info/GPTAPIKeyInfo';
+import GPTSearchInfo from './info/GPTSearchInfo';
 
 type DisconnectedPromptProps = {
   requirement?: RequirementStateType;
 };
 
 export default function DisconnectedPrompt({ requirement }: DisconnectedPromptProps) {
-  const { openModal, clearModal } = useModal();
-
-  const handleCodeCopy = async () => {
-    if (!requirement) return;
-    try {
-      await navigator.clipboard.writeText(requirement.title);
-      window.open('https://chat.openai.com', '_blank');
-    } catch (e) {
-      toast.error('링크 복사에 실패했습니다.');
-    }
-  };
-
-  const goToSetup = () => {
-    clearModal();
-    openModal({ type: ModalType.GPT_INFO });
-  };
-
   return (
-    <div css={disconnectedPromptStyle}>
-      <div className="api-container">
-        <p>GPT API key를 등록하면 요구사항에 대한 코드를 바로 받아볼 수 있어요.</p>
-        <button onClick={goToSetup}>등록하러 가기</button>
-      </div>
-      <div className="search-container">
-        <span>검색 버튼을 누르면 gpt에 검색할 내용을 만들어 드려요!</span>
-        <span>GPT 사이트가 열리면 붙여넣기를 해보세요</span>
-      </div>
+    <div css={disconnectedPromptStyle(theme)}>
+      {requirement?.gpt ? (
+        <CodeBlock code={requirement.gpt} />
+      ) : (
+        <>
+          <GPTAPIKeyInfo />
+          <GPTSearchInfo requirement={requirement} />
+        </>
+      )}
     </div>
   );
 }
 
-const disconnectedPromptStyle = css`
+const disconnectedPromptStyle = (theme: ThemeType) => css`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 30px;
 
-  button {
-    color: green;
+  h2 {
+    font-size: 1.2rem;
+    font-weight: 600;
   }
 
-  .api-container {
-    button {
-      padding-top: 10px;
-    }
+  .text-green {
+    color: ${theme.colors.green};
   }
 
-  .search-container {
+  .text-beige {
+    color: ${theme.colors.beige};
+  }
+
+  div {
     display: flex;
     flex-direction: column;
     gap: 10px;
