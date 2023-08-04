cc61a10ddc45f5164e1758018cbe7c6cc3388279
pySoo <soopy368@gmail.com>
2023-08-04 23:42:45 +0900

    [feat] : 타블렛 너비 이상이면 수평 정렬 되도록 media query 적용

--- src/components/KanbanBoard/IssueInfo.tsx
+++ src/components/KanbanBoard/IssueInfo.tsx
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
 
+import { theme, ThemeType } from '@/styles/theme';
+
 import DevEnvironmentForm from './DevEnvironmentForm';
 
 type IssueInfoProps = {
@@ -8,20 +10,28 @@ type IssueInfoProps = {
 
 export default function IssueInfo({ issueTitle }: IssueInfoProps) {
   return (
-    <section css={issueInfoStyle}>
+    <section css={issueInfoStyle(theme)}>
       <h2 className="issue-modal-title">{issueTitle}</h2>
       <DevEnvironmentForm />
     </section>
   );
 }
-const issueInfoStyle = css`
+const issueInfoStyle = (theme: ThemeType) => css`
+  width: 100%;
   display: flex;
   align-items: center;
-  gap: 20px;
-  padding: 10px 20px 5px 20px;
+  flex-direction: column;
+  gap: 10px;
+  padding: 10px 20px;
 
   .issue-modal-title {
     font-size: 1.2rem;
     font-weight: 700;
+  }
+
+  @media (min-width: ${theme.screens.md}px) {
+    flex-direction: row;
+    gap: 20px;
+    padding-bottom: 5px;
   }
 `;
