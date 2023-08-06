e701f3dd8ec07cae62e1db74b94d7593f7d4be5a
pySoo <soopy368@gmail.com>
2023-08-06 23:16:31 +0900

    [fix] : clipboard가 없는 환경인 경우 execCommand를 이용하여 복사하도록 수정

--- src/utils/gpt.ts
+++ src/utils/gpt.ts
@@ -1,6 +1,6 @@
 import { toast } from 'react-toastify';
 
-import { copyTextProps, generateSearchPromptProps } from '@/types/gpt';
+import { copyTextProps, generateSearchPromptProps, handleTextCopyProps } from '@/types/gpt';
 
 export const generateSearchPrompt = ({ title, framework, language }: generateSearchPromptProps) => {
   let sentence = '';
@@ -15,16 +15,41 @@ export const generateSearchPrompt = ({ title, framework, language }: generateSea
   return sentence;
 };
 
-export const copyText = async ({ text, toastMessage, isRequirement }: copyTextProps) => {
+const handleTextCopy = ({ callback, toastMessage, isRequirement }: handleTextCopyProps) => {
   try {
-    await navigator.clipboard.writeText(text);
+    callback();
+
     if (toastMessage) toast.success(toastMessage);
+
     if (isRequirement) {
-      setTimeout(() => {
-        window.open('https://chat.openai.com', '_blank');
-      }, 1200);
+      setTimeout(function () {
+        window.open('https://chat.openai.com');
+      }, 800);
     }
-  } catch (e) {
+  } catch (error) {
     toast.error('복사에 실패했어요. 다시 시도해 주세요.');
+  }
+};
+
+export const copyText = async ({ text, toastMessage, isRequirement }: copyTextProps) => {
+  if (navigator.clipboard) {
+    handleTextCopy({
+      callback: async () => await navigator.clipboard.writeText(text),
+      toastMessage,
+      isRequirement,
+    });
+  } else {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+
+    handleTextCopy({
+      callback: () => document.execCommand('copy'),
+      toastMessage,
+      isRequirement,
+    });
+
+    document.body.removeChild(textarea);
   }
 };
