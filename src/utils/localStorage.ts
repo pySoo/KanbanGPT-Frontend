669227f9ccec0d9f31dbe137028597e388b65255
pySoo <soopy368@gmail.com>
2023-12-10 19:56:17 +0900

    [refactor] : recoil AtomEffect 함수 내부 any 타입을 제거하고 제네릭 타입으로 교체

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,14 +1,19 @@
-import { AtomEffect } from 'recoil';
+import { AtomEffect, DefaultValue } from 'recoil';
 
-export const localStorageEffect: <T>(key: string) => AtomEffect<T> =
-  (key: string) =>
+export const localStorageEffect =
+  <T>(key: string): AtomEffect<T> =>
   ({ setSelf, onSet }) => {
     const savedValue = localStorage.getItem(key);
+
     if (savedValue !== null) {
       setSelf(JSON.parse(savedValue));
     }
 
-    onSet((newValue: any, _: any, isReset: boolean) => {
-      isReset ? localStorage.removeItem(key) : localStorage.setItem(key, JSON.stringify(newValue));
+    onSet((newValue: T, _: T | DefaultValue, isReset: boolean) => {
+      if (isReset) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, JSON.stringify(newValue));
+      }
     });
   };
